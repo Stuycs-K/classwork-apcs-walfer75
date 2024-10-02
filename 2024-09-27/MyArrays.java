88fce40597e7ce0da9845ab3a45e69272fc14527
Aydan Ferrao <aferrao60@nycstudents.net>
2024-10-02 16:39:11 -0400

    Wrote a test case for arrayToString

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,12 @@
+public class MyArrays {
+    public static String arrayToString(int[] nums) {
+        String result = "[";
+        for(int i = 0; i < nums.length  - 1; i++) {
+            result += 
+        }
+    }
+    public static void main(String[] args) {
+        int[] test1 = new int[] {2, 3, 4, 5};
+        System.out.println(arrayToString(test1));
+    }
+}
