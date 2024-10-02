eaee7568325cdd84c59a7534b9a24f6ed24acaf1
Aydan Ferrao <aferrao60@nycstudents.net>
2024-10-02 16:45:41 -0400

    Fixed a bug by adding a curly brace to end of array

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,10 +1,10 @@
 public class MyArrays {
     public static String arrayToString(int[] nums) {
-        String result = "[";
+        String result = "{";
         for(int i = 0; i < nums.length  - 1; i++) {
             result += nums[i] + ", ";
         }
-        result += nums[nums.length - 1];
+        result += nums[nums.length - 1] + "}";
         return result;
     }
 
