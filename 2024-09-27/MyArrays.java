fe62b5d93af144ffb9c9c880a07e071ef2824805
Aydan Ferrao <aferrao60@nycstudents.net>
2024-10-02 16:42:37 -0400

    Finished ArrayToString and compiled

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -2,9 +2,12 @@ public class MyArrays {
     public static String arrayToString(int[] nums) {
         String result = "[";
         for(int i = 0; i < nums.length  - 1; i++) {
-            result += 
+            result += nums[i] + ", ";
         }
+        result += nums[nums.length - 1];
+        return result;
     }
+
     public static void main(String[] args) {
         int[] test1 = new int[] {2, 3, 4, 5};
         System.out.println(arrayToString(test1));
