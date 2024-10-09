510271b87522d92eed1c3eb2b22cb5c16d6eb269
Aydan Ferrao <aferrao60@nycstudents.net>
2024-10-08 21:09:59 -0400

    Wrote htmlTable

--- 2024-10-07/ArrayDemo.java
+++ 2024-10-07/ArrayDemo.java
@@ -138,6 +138,14 @@ public class ArrayDemo{
   //   e.g. htmlTable(new int[][]{{1,2},{3}})  returns:
   // "<table><tr><td>1</td><td>2</td></tr><tr><td>3</td></tr></table>"
   public static String htmlTable(int[][]nums){
-    return "";
+    String result = "<table>";
+    for (int i = 0; i < nums.length; i++) {
+      result += "<tr>";
+      for (int j = 0; j < nums[i].length; j++) {
+        result += "<td>" + nums[i][j] + "</td>";
+      }
+      result += "</tr>";
+    }
+    return result + "</table>";
   }
 }
