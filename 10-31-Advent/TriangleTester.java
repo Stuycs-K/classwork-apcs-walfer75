65fb771e2bec539dd2c31896301c8a1d03b9d098
Aydan Ferrao <aydanf@nycstudents.net>
2024-10-31 11:16:28 -0400

    Added the TriangleTest and inputTri files

---
+++ 10-31-Advent/TriangleTester.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TriangleTest {
+  public static int countTrianglesA(String filename) {
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      while (input.hasNextLine()) {
+        
+      }
+    }
+  }
+}
