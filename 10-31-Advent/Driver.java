d853f8d9a710058127f06b21a734267c061ead94
Aydan Ferrao <aydanf@nycstudents.net>
2024-11-04 10:41:03 -0500

    added countTrianglesA method

--- 10-31-Advent/Driver.java
+++ 10-31-Advent/Driver.java
@@ -9,5 +9,6 @@ import java.util.ArrayList;
        total += sc2.nextDouble();
      }
      System.out.println(total);
+     System.out.println(TriangleTester.countTrianglesA("inputTri.txt"));
    }
  }
--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -2,14 +2,37 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class TriangleTest {
+public class TriangleTester {
   public static int countTrianglesA(String filename) {
+    int total = 0;
+
     try {
       File file = new File(filename);
       Scanner input = new Scanner(file);
+
       while (input.hasNextLine()) {
-        
+        int s1 = input.nextInt();
+        int s2 = input.nextInt();
+        int s3 = input.nextInt();
+        if (
+          ((s1 + s2) > s3) &&
+          ((s2 + s3) > s1) &&
+          ((s3 + s1) > s2)
+        ) {
+          total ++;
+        }
+        input.nextLine();
       }
+    }  catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return (0); //you can return from a void function just don't put a value.
     }
+    return(total);
+  }
+
+  public static int countTrianglesB(String filename){
+    int total = 0;
+
   }
 }
