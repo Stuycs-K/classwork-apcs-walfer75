f519393c96292574e2fc831363eeecdef9079eaf
Aydan Ferrao <aydanf@nycstudents.net>
2025-01-02 10:54:28 -0500

    Completed tests

--- 2025-01-02/Main.java
+++ 2025-01-02/Main.java
@@ -1,6 +1,9 @@
 public class Main {
   public static void main(String[] args) {
+    Fib i = new Fib();
     int n = Integer.parseInt(args[0]);
-    System.out.println(Fib.fibonacci(n));
+    System.out.println(i.fibonacci(n));
+    //46-47 under 10 seconds
+    //46 is the last before wraparound
   }
 }
