d2e4e03b1a3cf07680e36cf337359713c56a7172
Aydan Ferrao <aydanf@nycstudents.net>
2025-01-03 11:01:16 -0500

    Started the practice problem

--- 2025-01-02/Main.java
+++ 2025-01-02/Main.java
@@ -1,9 +1,13 @@
 public class Main {
   public static void main(String[] args) {
+    /*
     Fib i = new Fib();
     int n = Integer.parseInt(args[0]);
     System.out.println(i.fibonacci(n));
     //46-47 under 10 seconds
     //46 is the last before wraparound
+    */
+    Recursion a = new Recursion();
+    
   }
 }
---
+++ 2025-01-02/Recursion.java
@@ -0,0 +1,11 @@
+public class Recursion {
+
+  public static void makeLetters(int remainingLetters, String result, String alphabet) {
+    if (remainingLetters == 0) {
+      for (int i = 0; i < alphabet.length(); i++) {
+        
+      }
+    }
+
+  }
+}
